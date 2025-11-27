d51aec9e62963be1b1404179bc9716a482166154
Maximus7474 <the.maximus.7474@gmail.com>
2025-11-27 12:10:17 +0100

    feat(getChangelog): module to fetch the changed files

---
+++ src/getChangelog.ts
@@ -0,0 +1,75 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import type { FileChangelog } from '../types/core';
+
+export async function getFileChangelog() {
+  const GITHUB_TOKEN = core.getInput('github_token', { required: true });
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+  const { owner, repo } = github.context.repo;
+  const currentSha = github.context.sha;
+
+  core.info('Fetching latest tag...');
+
+  const tagsResponse = await octokit.rest.repos.listTags({
+    owner,
+    repo,
+    per_page: 1,
+  });
+
+  if (tagsResponse.data.length === 0) {
+    core.setFailed('No Git tags found in the repository.');
+    return;
+  }
+
+  const latestTag = tagsResponse.data[0]!;
+  const baseCommitSha = latestTag.commit.sha;
+
+  core.info(`Latest Tag Found: ${latestTag.name}`);
+  core.info(`Base Commit SHA (Tag): ${baseCommitSha}`);
+
+  // if the latest tag is the current commit, there are no new changes
+  if (baseCommitSha === currentSha) {
+    core.info('The latest tag is the current HEAD. No new commits to compare.');
+    return;
+  }
+
+  core.info(`Comparing commits between ${latestTag.name} and HEAD...`);
+
+  const comparisonResponse = await octokit.rest.repos.compareCommits({
+    owner,
+    repo,
+    base: baseCommitSha, // start from the tag commit
+    head: currentSha,    // go up to the current commit
+  });
+
+  const { files } = comparisonResponse.data;
+
+  const changedFiles: FileChangelog = {
+    added: [],
+    removed: [],
+    modified: [],
+  };
+
+  if (files) {
+    for (const file of files) {
+      if (file.status === 'added')
+        changedFiles.added.push(file.filename);
+      else if (file.status === 'removed')
+        changedFiles.removed.push(file.filename);
+      else if (['modified', 'changed'].includes(file.status))
+        changedFiles.modified.push(file.filename);
+      else if (file.status === 'renamed' && file.previous_filename) {
+        const oldFile = file.previous_filename,
+          newFile = file.filename;
+
+        changedFiles.removed.push(oldFile);
+        changedFiles.added.push(newFile);
+      }
+    }
+
+    return { changedFiles, version: latestTag.name };
+  } else {
+    core.info('No changes found, exiting...')
+    return;
+  }
+}
